844f1865bfc30cca4c18a891ccfa2e443d0b2fbd
Alexander Watts <acwatts.dev@gmail.com>
2021-10-11 20:23:03 +0100

    Added user component to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Users from './Users';
+import User from './User';
 
 const App = () => (
   <>
     <h1>Demo</h1>
     <Users />
+    <User id={10} />
   </>
 );
 
