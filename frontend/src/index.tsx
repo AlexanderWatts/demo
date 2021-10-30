6a59f40feafdf5f74e46694ca2691e91c57bc80d
Alexander Watts <acwatts.dev@gmail.com>
2021-10-30 02:18:44 +0100

    Added supervisor load graph to index page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Users from './Users';
 import User from './User';
+import LoadGraph from './LoadGraph';
 
 const App = () => (
   <>
     <h1>Demo</h1>
+    <LoadGraph />
     <Users />
     <User id={2} />
   </>
