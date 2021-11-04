91559a96c6a8ef8691c71d103f0f8a2669dee482
Alexander Watts <acwatts.dev@gmail.com>
2021-11-04 10:30:00 +0000

    Added template file covering some of the most useful things to know, this will be filled out interactively as part of demo

---
+++ frontend/src/toComplete.tsx
@@ -0,0 +1,53 @@
+// Import react to this file in order to use it
+import React from 'react';
+
+/**
+ * Functions
+ * 
+ * - Function declarations
+ * - Arrow functions
+ */
+
+
+/**
+ * Passing data to functions
+ * 
+ * - Pass in arguments to functions
+ * - Object destructuring
+ */
+
+
+/**
+ * Functional React components
+ * 
+ *  - Create basic component
+ *  - Export component to use it
+ *  - How to use component 
+ *  - React hooks for managing state
+ */
+
+
+/**
+ * Intro to TypeScript
+ * 
+ * - Specifying types to our data
+ * - Using TypeScript with our functions 
+ * - Generic funcitons
+ */
+
+
+/**
+ * Functional React component lifecylce
+ * 
+ *  - Update state
+ *  - useEffect hook
+ *  - Fetching data online
+ */
+
+
+/**
+ * Installing and using external packages
+ * 
+ * - Install a package and use it
+ */
+
